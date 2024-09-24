b4a01eb70d5dcd4d0824c95c99fac90876424ad2
LEVISTKL <112727938+LEVISTKL@users.noreply.github.com>
2024-09-25 00:26:49 +0200

    save metodus 1/3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,19 +12,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Nepesseg_GRF_HF
 {
     public partial class MainWindow : Window
     {
+        static List<Adat> adatok = new List<Adat>();
         public MainWindow()
         {
             InitializeComponent();
+            beolvasas();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        public static void beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok-utf8.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                adatok.Add(new Adat(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    class Adat
+    {
+        public string Orszag;
+        public int Terulet;
+        public int Nepesseg;
+        public string FoVaros;
+        public int FoVarosNepesseg;
+
+        public Adat(string sor)
+        {
+            string[] tomb = sor.Split(';');
+            Orszag = tomb[0];
+            Terulet = int.Parse(tomb[1]);
+            Nepesseg = int.Parse(tomb[2].Contains("g") ? tomb[2].Replace("g", "0000") : tomb[2]);
+            FoVaros = tomb[3];
+            FoVarosNepesseg = int.Parse(tomb[4]) * 1000;
+        }
+
+        public double nepsuruseg()
+        {
+            return Math.Round(Convert.ToDouble(Nepesseg) / Convert.ToDouble(Terulet), 0);
+        }
+
+        public bool FoVaros30_koncentracio()
+        {
+            return Convert.ToDouble(Nepesseg) * 0.3 < FoVarosNepesseg;
         }
     }
 }
