45c6379ed38cc34350598ef52c5d62487e1edd08
LEVISTKL <112727938+LEVISTKL@users.noreply.github.com>
2024-09-25 00:53:06 +0200

    kész

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,7 +32,8 @@ namespace Nepesseg_GRF_HF
 
         public static void beolvasas()
         {
-            StreamReader sr = new StreamReader("adatok-utf8.txt");
+            
+            StreamReader sr = new StreamReader(File.Exists("ujadat.txt") ? "ujadat.txt":"adatok-utf8.txt");
             sr.ReadLine();
             while (!sr.EndOfStream)
             {
@@ -40,9 +41,40 @@ namespace Nepesseg_GRF_HF
             }
             sr.Close();
         }
+        public static void mentes(string newData)
+        {
+            Adat adat = new Adat(newData);
+            adatok.Add(adat);
+            StreamWriter sw = new StreamWriter("ujadat.txt");
+            foreach (var item in adatok)
+            {
+                sw.WriteLine($"{item.Orszag};{item.Terulet};{item.Nepesseg};{item.FoVaros};{item.FoVarosNepesseg}");
+            }
+            sw.Close();
+        }
 
         private void Save(object sender, RoutedEventArgs e)
         {
+            hibauzenet.Text = "";
+            if (orszag_text.Text == "" || fovaros_text.Text == "" || terulet_text.Text == "" || nepesseg_text.Text == "" || fovaros_text.Text == "")
+            {
+                hibauzenet.Text = "Adjon meg adatokat";
+            }
+            else
+            {
+
+                if (int.Parse(fovaros_lakosssag_text.Text) > int.Parse(nepesseg_text.Text))
+                {
+                    hibauzenet.Text = "A főváros lakossága nem lehet nagyobb az orszag lakossaganal";
+                    nepesseg_text.Text = fovaros_lakosssag_text.Text;
+                }
+                else
+                {
+                    mentes($"{orszag_text.Text};{terulet_text.Text};{nepesseg_text.Text};{fovaros_text.Text};{fovaros_lakosssag_text.Text}");
+                    hibauzenet.Text = "mentés sikeres";
+                }
+
+            }
 
         }
     }
